0b689bd7d6dbd84dda85a1e5f824489480944d10
Maximilian Faust <BsHr_Faust@web.de>
2022-05-11 00:03:50 +0200

    creating shitty test app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "React"
 
 function App() {
+  const [message, setMessage] = useState("")
+  useEffect(() => {
+    fetch("https://heroku-test-app-wd-matrix.herokuapp.com/")
+    .then(response => {
+      return (response.ok ? response.json(): "fuck you, not working!")
+    })
+    .then(data => {
+      setMessage(data)
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +20,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Is the shit working?</p>
+        <p>Answer:</p>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
